f8236f4371602fd6a8d8889407ae8156d6f076aa
marcel0ll <marcelo@lotuz.dev>
2021-01-09 16:49:56 -0300

    Remove debug flag inside visitor

--- tree-sitter-visitor.c
+++ tree-sitter-visitor.c
@@ -10,7 +10,6 @@ struct visit_context {
 };
 
 struct visitor {
-  char * flag;
   void * enter;
   void * exit;
 };
@@ -31,13 +30,6 @@ struct visit_context * context_new(TSLanguage * language, const char * source, i
 }
 
 void context_delete(struct visit_context * context) {
-  uint32_t visitor_count = ts_language_symbol_count(context->language);
-  uint32_t i;
-  for(i = 0; i < visitor_count; i++ ) {
-    if (context->visitors[i] != NULL) {
-      free(context->visitors[i]);
-    }
-  }
   free(context->visitors);
   free((char *)context->source);
   free(context);
@@ -53,7 +45,6 @@ bool context_set_type_visitor(struct visit_context * context, const char * type,
   struct visitor * v = malloc(sizeof(struct visitor));
   v->enter = enter;
   v->exit = exit;
-  v->flag = "one";
   context->visitors[i] = v;
   return true;
 }
@@ -64,7 +55,6 @@ void context_set_types_visitor(struct visit_context * context, const char * type
   struct visitor * v = malloc(sizeof(struct visitor));
   v->enter = enter;
   v->exit = exit;
-  v->flag = "mult";
 
   for (int i = 0; (type = types[i]) != NULL; i++) {
     TSFieldId i = ts_language_symbol_for_name(context->language, type, strlen(type), true);
